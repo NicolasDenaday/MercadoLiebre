571072a7cf77a765e9adba140f0423ba15fa265e
NicolasDenaday <nicodenaday@gmail.com>
2022-08-24 23:05:23 -0300

    Update MercadoLiebre

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const app= express();
 
 app.use(express.static(path.join(__dirname,"public")));
 
-app.listen(3000, () => console.log("Servidor corriendo"));
+app.listen(3000, ()=>{
+    console.log("Servidor corriendo")
+});
 
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+app.get("/register", function(req,res){
+    res.sendFile(path.join(__dirname, "/views/register.html"))
 });
