37e3c421d7429ddb421623a8a5bcd683b6000d4b
NicolasDenaday <nicodenaday@gmail.com>
2022-08-29 20:13:35 -0300

    Update MercadoLiebre

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app= express();
 
 app.use(express.static(path.join(__dirname,"public")));
 
-app.listen(3000, ()=>{
+app.listen(proces.env.PORT || 3000, ()=>{
     console.log("Servidor corriendo")
 });
 
